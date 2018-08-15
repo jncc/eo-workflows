5e882bfa4763277da4732a9e9bd876c0af1ed8de
Cathy Jin <cathy.jin@jncc.gov.uk>
2018-08-15 14:52:38 +0000

    Changing workflow to not use baskets and added common RunJobs task

--- process_s1_range_with_retries/SubmitJobs.py
+++ process_s1_range_with_retries/SubmitJobs.py
@@ -7,109 +7,58 @@ import stat
 import glob
 import workflow_common.common as wc
 import re
+import random
 from luigi.util import requires
-from process_s1_range_with_retries.PrepareBasket import PrepareBasket
+from process_s1_range_with_retries.GetProductsToProcessList import GetProductsToProcessList
+from workflow_common.RunJob import RunJob
 from os.path import join
 
 log = logging.getLogger('luigi-interface')
 
-@requires(PrepareBasket)
+@requires(GetProductsToProcessList)
 class SubmitJobs(luigi.Task):
     pathRoots = luigi.DictParameter()
     outputFile = luigi.Parameter()
     maxScenes = luigi.IntParameter()
     startDate = luigi.DateParameter()
     endDate = luigi.DateParameter()
-
-    def createSingularityScript(self, inputFile, processingFileRoot, stateFileRoot, singularityScriptPath):
-        demDir = self.pathRoots["demDir"]
-        outputDir = self.pathRoots["outputDir"]
-        singularityDir = self.pathRoots["singularityDir"]
-        singularityImgDir = self.pathRoots["singularityImgDir"]
-
-        realRawDir = os.path.dirname(os.path.realpath(inputFile))
-        basketDir = os.path.dirname(inputFile)
-        rawFilename = os.path.basename(inputFile)
-        productId = wc.getProductIdFromLocalSourceFile(inputFile)
-
-        singularityCmd = "{}/singularity exec --bind {}:/data/sentinel/1 --bind {}:/data/states --bind {}:/data/raw --bind {}:/data/dem --bind {}:/data/processed --bind {}:/data/basket {}/s1-ard-processor.simg /app/exec.sh --productId {} --sourceFile '/data/raw/{}' --outputFile '{}'" \
-            .format(singularityDir,
-                processingFileRoot,
-                stateFileRoot,
-                realRawDir,
-                demDir,
-                outputDir,
-                basketDir,
-                singularityImgDir,
-                productId,
-                rawFilename,
-                self.outputFile)
-        
-        with open(singularityScriptPath, 'w') as singularityScript:
-            singularityScript.write(singularityCmd)
-
-        st = os.stat(singularityScriptPath)
-        os.chmod(singularityScriptPath, st.st_mode | 0o110 )
-
-        log.info("Created run_singularity_workflow.sh for " + inputFile + " with command " + singularityCmd)
+    outputFilePattern = luigi.Parameter()
+    testProcessing = luigi.BoolParameter(default = False)
 
     def run(self):
-        with self.input().open('r') as inputFile:
-            contents = json.load(inputFile)
+        with self.input().open('r') as stateFile:
+            contents = json.load(stateFile)
 
-        inputDir = contents["basketPath"]
-        processingDir = self.pathRoots["processingDir"]
+        submittedProducts = {
+            "queryWindow": {
+                "start": str(self.startDate),
+                "end": str(self.endDate)
+            },
+            "maxScenes": self.maxScenes,
+            "products": []
+        }
 
-        for inputFile in glob.glob(os.path.join(inputDir, "*.zip")):
-            log.info("Found " + inputFile + ", setting up directories")
+        tasks = []
+        for product in contents["products"]:
+            task = RunJob(
+                inputFile = product["filepath"],
+                outputFilePattern = self.outputFilePattern,
+                pathRoots = self.pathRoots,
+                removeSourceFile = True,
+                testProcessing = self.testProcessing
+            )
 
-            filename = os.path.basename(os.path.splitext(inputFile)[0])
+            tasks.append(task)
 
-            workspaceRoot = os.path.join(processingDir, filename)
-            
-            processingFileRoot = os.path.join(workspaceRoot, "processing")
-            if not os.path.exists(processingFileRoot):
-                os.makedirs(processingFileRoot)
+        yield tasks
 
-            stateFileRoot = os.path.join(workspaceRoot, "states")
-            if not os.path.exists(stateFileRoot):
-                os.makedirs(stateFileRoot)
+        for task in tasks:
+            with task.output().open('r') as taskOutput:
+                submittedProduct = json.load(taskOutput)
+                submittedProducts["products"].append(submittedProduct)
 
-            singularityScriptPath = os.path.join(workspaceRoot, "run_singularity_workflow.sh")
-            if not os.path.isfile(singularityScriptPath):
-                self.createSingularityScript(inputFile, processingFileRoot, stateFileRoot, singularityScriptPath)
-
-            lotusCmd = "bsub -q short-serial -R 'rusage[mem=18000]' -M 18000 -W 10:00 -o {}/%J.out -e {}/%J.err {}" \
-                .format(
-                    workspaceRoot,
-                    workspaceRoot,
-                    singularityScriptPath
-                )
-
-            output = {
-                "jobIds": list()
-            }
-
-            try:
-                output = subprocess.check_output(
-                    lotusCmd,
-                    stderr=subprocess.STDOUT,
-                    shell=True)
-
-                regex = '[0-9]{7,}' # job ID is 7 digits
-                match = re.search(regex, output.decode("utf-8"))
-                jobId = match.group(0)
-
-                log.info("Successfully submitted lotus job <%s> for %s using command: %s", jobId, inputFile, lotusCmd)
-
-                output["jobIds"][filename] = jobId
-            except subprocess.CalledProcessError as e:
-                errStr = "command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output)
-                log.error(errStr)
-                raise RuntimeError(errStr)
-
-            with self.output().open('w') as out:
-                out.write(json.dumps(output))
+        with self.output().open("w") as outFile:
+            outFile.write(json.dumps(submittedProducts))
 
     def output(self):
         outputFolder = self.pathRoots["processingDir"]
